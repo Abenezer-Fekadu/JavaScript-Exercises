1d8050061eb1f49d52410b0823682447ea75aa6d
Abenezer Fekadu <se.abenezer.fekadu@gmail.com>
2021-01-27 22:40:46 +0300

    Some changes to the app js file is done.

--- Lesson 1 [Lab 04]/PersonalDatabase/assets/js/app.js
+++ Lesson 1 [Lab 04]/PersonalDatabase/assets/js/app.js
@@ -17,27 +17,50 @@ let numberOfFamily;
 //height and weight 
 let weight;
 let height;
-
+let wightStatus;
 //bmi calculator 
-let calcBmi = function(weight, height) {
+let tempBmi = function(weight, height){
 
     let bmi = weight / (height * height);
+        bmi = Number(bmi.toFixed(1));
+        
+        
+	if (bmi <= 18.5){
+    		weightStatus = "Underweight";
+	}
+	else if (18.5 > bmi <= 24.9){
+	    weightStatus = "Normal or Healthy Weight";
+	}			
+	else if (25 >= bmi <=29.9 ){
+    		weightStatus = "Overweight";
+	}
+	else{
+    		weightStatus = "Obese";
+	}
+	
+	return bmi;
+};
 
 
-    bmi = Number(bmi.toFixed(1));
-
-    console.log("Your Bmi is : " + bmi);
-    if (bmi < 18.5)
-        console.log("Your are : Underweight");
-    else if (bmi >= 18.5 && bmi <= 24.9)
-        console.log("Your are : Normal");
-    else if (bmi >= 25.0 && bmi <= 29.9)
-        console.log("Your are : Overweight");
-    else if (bmi >= 30.0)
-        console.log("Your are : Obese");
+//let calcBmi = function(weight, height) {
+//
+   // let bmi = weight / (height * height);
 
 
-}
+  //  bmi = Number(bmi.toFixed(1));
+
+    //console.log("Your Bmi is : " + bmi);
+   // if (bmi < 18.5)
+     //   console.log("Your are : Underweight");
+   // else if (bmi >= 18.5 && bmi <= 24.9)
+     //   console.log("Your are : Normal");
+   // else if (bmi >= 25.0 && bmi <= 29.9)
+       // console.log("Your are : Overweight");
+   // else if (bmi >= 30.0)
+       // console.log("Your are : Obese");
+
+
+//}
 
 //age calculator 
 function ageCalc(birthYear) {
@@ -91,6 +114,8 @@ if (tempAge >= 18) {
     });
 
     // call bmi calculator 
-    calcBmi(weight, height);
+    console.log(height);
+    console.log(weight);
+    console.log("Your weight is " + tempBmi(weight, height) + " " + "and Status: " + weightStatus);
 
 })();
