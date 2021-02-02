e5fb792905fe0802a44f132e86c2f994f8086a39
Abenezer Fekadu <se.abenezer.fekadu@gmail.com>
2021-02-02 21:14:02 +0300

    The Search and ordering of ascending and discending based on date is completed.

--- Lesson 2[Lab 05]/Starter/assets/js/app.js
+++ Lesson 2[Lab 05]/Starter/assets/js/app.js
@@ -9,8 +9,8 @@ const filter = document.querySelector('#filter');                      //the tas
 const taskList = document.querySelector('.collection');          //The ul
 
 const clearBtn = document.querySelector('.clear-tasks');      //the all task clear button
-//the reload button at the top right of navigation
-const reloadIcon = document.querySelector('.fa');   
+
+const reloadIcon = document.querySelector('.fa');               //the reload button at the top right of navigation
 
 const assend = document.querySelector('#assend');
 
@@ -25,7 +25,7 @@ form.addEventListener('submit', addNewTask);
 clearBtn.addEventListener('click', clearAllTasks);
 
 //   Filter Task 
-// filter.addEventListener('keyup', filterTasks);
+filter.addEventListener('keyup', filterTasks);
 
    // Remove task event [event delegation]
 taskList.addEventListener('click', removeTask);
@@ -34,15 +34,10 @@ taskList.addEventListener('click', removeTask);
 reloadIcon.addEventListener('click', reloadPage);
 
 // Even tistner for Assending
-// assend.addEventListener('click', assending);
+assend.addEventListener('click', assending);
 
 // Event Listner For Descending
-// desend.addEventListener('click', desending);
-
-// dropdown Event Listner
-// dropbtn.addEventListener('click', order);
-
-
+desend.addEventListener('click', desending);
 
 
 
@@ -51,6 +46,7 @@ function reloadPage() {
     //using the reload fun on location object 
     location.reload();
 }
+
 // Add New  Task Function definition 
 function addNewTask(e) {
 
@@ -92,10 +88,67 @@ function clearAllTasks(e) {
         taskList.removeChild(taskList.firstChild);
     }
 }
+
 // Filter tasks function definition 
+function filterTasks(e) {
+    let userdata = e.target.value;
+    let coll = taskList.querySelectorAll(".collection-item");
+    // console.log(userdata);
+    for(i = 0; i < coll.length; i++){
+        // coll[i].style.color = "black"
+        if (coll[i].firstChild.textContent.indexOf(userdata) == -1){
+            coll[i].style.display = "none";
+        }else if(coll[i].firstChild.textContent.indexOf(userdata) == 0){
+            coll[i].style.display = "block";
+        }
+    }
+
+}
 
 
+// Sorting Order
+function order(e){
+    let div = document.querySelectorAll(".collection-item");
+    listDate = new Array();
+    for (i = 0; i < div.length; i++) {
+        ld = new Date(div[i].children[1].textContent);
+        listDate.push(ld.getTime());
+    }
+    let newlist = listDate.sort((a, b) => a - b);
+    // console.log(newlist);
+    return newlist;
+}
 
+function assending(e){
+    let orderList = order();
+    let doc = document.querySelectorAll(".collection-item");
+    for (i = 0; i < orderList.length; i++) {
+        for (j = 0; j < doc.length; j++){
+            d = doc[j].children[1].textContent;
+            nd = new Date(doc[j].children[1].textContent);
+            if (orderList[i] === nd.getTime()){
+                let a = doc[j].innerHTML;
+                doc[j].innerHTML = doc[i].innerHTML;
+                doc[i].innerHTML = a;
+            }
+        }   
+    }
+}
+function desending(e){
+    let orderdList = order().reverse();
+    let doc = document.querySelectorAll(".collection-item");
+    for (i = 0; i < orderdList.length; i++) {
+        for (j = 0; j < doc.length; j++){
+            d = doc[j].children[1].textContent;
+            nd = new Date(doc[j].children[1].textContent);
+            if (orderdList[i] === nd.getTime()){
+                let a = doc[j].innerHTML;
+                doc[j].innerHTML = doc[i].innerHTML;
+                doc[i].innerHTML = a;
+            }
+        }
+    }   
+}
 
 // Remove Task function definition 
 function removeTask(e) {
